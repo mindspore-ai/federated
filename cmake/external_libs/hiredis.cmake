c391c095e4c0fe38748149322ac7e20a5ebc8216
zhanzhan1 <zhanzhan1@huawei.com>
2023-02-03 11:37:53 +0800

    MD5 to SHA256

--- cmake/external_libs/hiredis.cmake
+++ cmake/external_libs/hiredis.cmake
@@ -2,17 +2,17 @@ set(hiredis_CXXFLAGS "-Wl,-z,now -s")
 set(hiredis_CFLAGS "-Wl,-z,now -s")
 if(ENABLE_GITEE)
     set(REQ_URL "https://gitee.com/mirrors/hiredis/repository/archive/release/v1.0.2.tar.gz")
-    set(MD5 "20acecf5cb87868723f97a9bbb5cf045")
+    set(SHA256 "4d5b92dd338f7c2157844892bc8098a7f966e67b6fa2e75b0b398d3fe5c90eea")
 else()
     set(REQ_URL "https://github.com/redis/hiredis/archive/refs/tags/v1.0.2.tar.gz")
-    set(MD5 "58e8313188f66ed1be1c220d14a7752e")
+    set(SHA256 "e0ab696e2f07deb4252dda45b703d09854e53b9703c7d52182ce5a22616c3819")
 endif()
 
 mindspore_add_pkg(hiredis
         VER 1.0.2
         LIBS hiredis hiredis_ssl
         URL ${REQ_URL}
-        MD5 ${MD5}
+        SHA256 ${SHA256}
         CMAKE_OPTION -DCMAKE_BUILD_TYPE:STRING=Release
         -DENABLE_SSL:BOOL=ON
         -DDISABLE_TESTS:BOOL=ON -DOPENSSL_ROOT_DIR:PATH=${openssl_ROOT})
