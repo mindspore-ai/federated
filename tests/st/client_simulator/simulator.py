8dcf8f9921d3ed4efedfa1ec2aad2bfff3a5a6a9
twc <tanweicheng@huawei.com>
2023-02-06 14:52:55 +0800

    client simulator realize communication comresssion

--- tests/st/client_simulator/simulator.py
+++ tests/st/client_simulator/simulator.py
@@ -22,12 +22,11 @@ import sys
 import requests
 from mindspore.fl.schema import (RequestFLJob, ResponseFLJob, ResponseCode,
                                  RequestUpdateModel, ResponseUpdateModel,
-                                 FeatureMap, RequestGetModel, ResponseGetModel)
+                                 FeatureMap, RequestGetModel, ResponseGetModel, CompressFeatureMap)
 import flatbuffers
 import numpy as np
 
 sys.path.append("../../../ut/python/tests")
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--pid", type=int, default=0)
@@ -53,6 +52,9 @@ random_fl_id = str(random.sample(alphabet, 8))
 server_not_available_rsp = ["The cluster is in safemode.",
                             "The server's training job is disabled or finished."]
 
+compress_type_str_map = {0: "NO_COMPRESS", 1: "DIFF_SPARSE_QUANT", 2: "QUANT"}
+
+
 def build_start_fl_job():
     """
     build start fl job
@@ -63,17 +65,65 @@ def build_start_fl_job():
     fl_id = start_fl_job_builder.CreateString(random_fl_id)
     timestamp = start_fl_job_builder.CreateString('2020/11/16/19/18')
 
+    download_compress_types = [0, 2]
+    RequestFLJob.RequestFLJobStartDownloadCompressTypesVector(start_fl_job_builder, len(download_compress_types))
+    for download_compress_type in reversed(download_compress_types):
+        start_fl_job_builder.PrependInt8(download_compress_type)
+    download_compress_types_off = start_fl_job_builder.EndVector()
+
     RequestFLJob.RequestFLJobStart(start_fl_job_builder)
     RequestFLJob.RequestFLJobAddFlName(start_fl_job_builder, fl_name)
     RequestFLJob.RequestFLJobAddFlId(start_fl_job_builder, fl_id)
     RequestFLJob.RequestFLJobAddDataSize(start_fl_job_builder, data_size)
     RequestFLJob.RequestFLJobAddEvalDataSize(start_fl_job_builder, eval_data_size)
     RequestFLJob.RequestFLJobAddTimestamp(start_fl_job_builder, timestamp)
+    RequestFLJob.RequestFLJobAddDownloadCompressTypes(start_fl_job_builder, download_compress_types_off)
     fl_job_req = RequestFLJob.RequestFLJobEnd(start_fl_job_builder)
 
     start_fl_job_builder.Finish(fl_job_req)
     buf = start_fl_job_builder.Output()
     return buf
+
+
+def construct_mask_array(param_num, upload_sparse_rate):
+    retain_num = int(param_num * upload_sparse_rate)
+    return [1 for _ in range(retain_num)] + [0 for _ in range(param_num - retain_num)]
+
+
+def build_compress_feature_map(builder, feature_map_temp, upload_sparse_rate):
+    """
+    build compress feature map
+    """
+    if not feature_map_temp:
+        return None
+    compress_feature_maps = list()
+    name_vec = list()
+    param_num = 0
+    for _, compress_feature_map in feature_map_temp.items():
+        param_num += len(compress_feature_map)
+    mask_array = construct_mask_array(param_num, upload_sparse_rate)
+
+    index = 0
+    for name, compress_feature_map in feature_map_temp.items():
+        length = len(compress_feature_map)
+        weight_full_name = builder.CreateString(name)
+        CompressFeatureMap.CompressFeatureMapStartCompressDataVector(builder, length)
+        weight = np.ones(length, dtype=np.int8)
+        for idx in range(length - 1, -1, -1):
+            if mask_array[index] == 1:
+                builder.PrependInt8(weight[idx])
+            index += 1
+        data = builder.EndVector()
+        CompressFeatureMap.CompressFeatureMapStart(builder)
+        CompressFeatureMap.CompressFeatureMapAddCompressData(builder, data)
+        CompressFeatureMap.CompressFeatureMapAddWeightFullname(builder, weight_full_name)
+        CompressFeatureMap.CompressFeatureMapAddMaxVal(builder, 1)
+        CompressFeatureMap.CompressFeatureMapAddMinVal(builder, 1)
+        compress_feature_map = CompressFeatureMap.CompressFeatureMapEnd(builder)
+        compress_feature_maps.append(compress_feature_map)
+        feature_name = builder.CreateString(name)
+        name_vec.append(feature_name)
+    return compress_feature_maps, name_vec
 
 
 def build_feature_map(builder, feature_map_temp):
@@ -83,13 +133,11 @@ def build_feature_map(builder, feature_map_temp):
     if not feature_map_temp:
         return None
     feature_maps = []
-    np_data = []
     for name, data in feature_map_temp.items():
         length = len(data)
         weight_full_name = builder.CreateString(name)
         FeatureMap.FeatureMapStartDataVector(builder, length)
         weight = np.random.rand(length) * 32
-        np_data.append(weight)
         for idx in range(length - 1, -1, -1):
             builder.PrependFloat32(weight[idx])
         data = builder.EndVector()
@@ -98,7 +146,45 @@ def build_feature_map(builder, feature_map_temp):
         FeatureMap.FeatureMapAddWeightFullname(builder, weight_full_name)
         feature_map = FeatureMap.FeatureMapEnd(builder)
         feature_maps.append(feature_map)
-    return feature_maps, np_data
+    return feature_maps
+
+
+def build_compress_update_model(iteration, feature_map_temp, upload_compress_type, upload_sparse_rate):
+    """
+    build updating model
+    """
+    builder_update_model = flatbuffers.Builder(1)
+    fl_name = builder_update_model.CreateString('fl_test_job')
+    fl_id = builder_update_model.CreateString(random_fl_id)
+    timestamp = builder_update_model.CreateString('2020/11/16/19/18')
+    compress_feature_maps, name_vec = build_compress_feature_map(builder_update_model, feature_map_temp,
+                                                                 upload_sparse_rate)
+    RequestUpdateModel.RequestUpdateModelStartCompressFeatureMapVector(builder_update_model, len(compress_feature_maps))
+    for single_feature_map in compress_feature_maps:
+        builder_update_model.PrependUOffsetTRelative(single_feature_map)
+    fbs_compress_feature_maps = builder_update_model.EndVector()
+
+    RequestUpdateModel.RequestUpdateModelStartNameVecVector(builder_update_model, len(name_vec))
+    for name in reversed(name_vec):
+        builder_update_model.PrependUOffsetTRelative(name)
+    fbs_name_vec = builder_update_model.EndVector()
+
+    RequestUpdateModel.RequestUpdateModelStart(builder_update_model)
+    RequestUpdateModel.RequestUpdateModelAddFlName(builder_update_model, fl_name)
+    RequestUpdateModel.RequestUpdateModelAddFlId(builder_update_model, fl_id)
+    RequestUpdateModel.RequestUpdateModelAddIteration(builder_update_model, iteration)
+    RequestUpdateModel.RequestUpdateModelAddCompressFeatureMap(builder_update_model, fbs_compress_feature_maps)
+    RequestUpdateModel.RequestUpdateModelAddTimestamp(builder_update_model, timestamp)
+    RequestUpdateModel.RequestUpdateModelAddUploadLoss(builder_update_model, upload_loss)
+    RequestUpdateModel.RequestUpdateModelAddUploadAccuracy(builder_update_model, upload_accuracy)
+
+    RequestUpdateModel.RequestUpdateModelAddNameVec(builder_update_model, fbs_name_vec)
+    RequestUpdateModel.RequestUpdateModelAddUploadCompressType(builder_update_model, upload_compress_type)
+    RequestUpdateModel.RequestUpdateModelAddUploadSparseRate(builder_update_model, upload_sparse_rate)
+    req_update_model = RequestUpdateModel.RequestUpdateModelEnd(builder_update_model)
+    builder_update_model.Finish(req_update_model)
+    buf = builder_update_model.Output()
+    return buf
 
 
 def build_update_model(iteration, feature_map_temp):
@@ -109,26 +195,24 @@ def build_update_model(iteration, feature_map_temp):
     fl_name = builder_update_model.CreateString('fl_test_job')
     fl_id = builder_update_model.CreateString(random_fl_id)
     timestamp = builder_update_model.CreateString('2020/11/16/19/18')
-
-    feature_maps, np_data = build_feature_map(builder_update_model, feature_map_temp)
-
+    feature_maps = build_feature_map(builder_update_model, feature_map_temp)
     RequestUpdateModel.RequestUpdateModelStartFeatureMapVector(builder_update_model, len(feature_map_temp))
     for single_feature_map in feature_maps:
         builder_update_model.PrependUOffsetTRelative(single_feature_map)
-    feature_map = builder_update_model.EndVector()
+    fbs_feature_map = builder_update_model.EndVector()
 
     RequestUpdateModel.RequestUpdateModelStart(builder_update_model)
     RequestUpdateModel.RequestUpdateModelAddFlName(builder_update_model, fl_name)
     RequestUpdateModel.RequestUpdateModelAddFlId(builder_update_model, fl_id)
     RequestUpdateModel.RequestUpdateModelAddIteration(builder_update_model, iteration)
-    RequestUpdateModel.RequestUpdateModelAddFeatureMap(builder_update_model, feature_map)
+    RequestUpdateModel.RequestUpdateModelAddFeatureMap(builder_update_model, fbs_feature_map)
     RequestUpdateModel.RequestUpdateModelAddTimestamp(builder_update_model, timestamp)
     RequestUpdateModel.RequestUpdateModelAddUploadLoss(builder_update_model, upload_loss)
     RequestUpdateModel.RequestUpdateModelAddUploadAccuracy(builder_update_model, upload_accuracy)
     req_update_model = RequestUpdateModel.RequestUpdateModelEnd(builder_update_model)
     builder_update_model.Finish(req_update_model)
     buf = builder_update_model.Output()
-    return buf, np_data
+    return buf
 
 
 def build_get_model(iteration):
@@ -139,10 +223,17 @@ def build_get_model(iteration):
     fl_name = builder_get_model.CreateString('fl_test_job')
     timestamp = builder_get_model.CreateString('2020/12/16/19/18')
 
+    download_compress_types = [0, 2]
+    RequestGetModel.RequestGetModelStartDownloadCompressTypesVector(builder_get_model, len(download_compress_types))
+    for download_compress_type in reversed(download_compress_types):
+        builder_get_model.PrependInt8(download_compress_type)
+    fbs_download_compress_types = builder_get_model.EndVector()
+
     RequestGetModel.RequestGetModelStart(builder_get_model)
     RequestGetModel.RequestGetModelAddFlName(builder_get_model, fl_name)
     RequestGetModel.RequestGetModelAddIteration(builder_get_model, iteration)
     RequestGetModel.RequestGetModelAddTimestamp(builder_get_model, timestamp)
+    RequestGetModel.RequestGetModelAddDownloadCompressTypes(builder_get_model, fbs_download_compress_types)
     req_get_model = RequestGetModel.RequestGetModelEnd(builder_get_model)
     builder_get_model.Finish(req_get_model)
     buf = builder_get_model.Output()
@@ -167,7 +258,6 @@ def datetime_to_timestamp(datetime_obj):
 
 session = requests.Session()
 current_iteration = 1
-np.random.seed(0)
 
 
 def start_fl_job():
@@ -185,6 +275,7 @@ def start_fl_job():
         print("Start fl job when safemode.")
         return start_fl_job_result, iteration
 
+    print("Start fl job response size: ", len(x.content))
     rsp_fl_job = ResponseFLJob.ResponseFLJob.GetRootAsResponseFLJob(x.content, 0)
     iteration = rsp_fl_job.Iteration()
     rsp_feature_map = {}
@@ -198,28 +289,47 @@ def start_fl_job():
     else:
         start_fl_job_result['reason'] = "Success"
         start_fl_job_result['next_ts'] = 0
-        for i in range(0, rsp_fl_job.FeatureMapLength()):
-            rsp_feature_map[rsp_fl_job.FeatureMap(i).WeightFullname()] = rsp_fl_job.FeatureMap(i).DataAsNumpy()
-            print("start fl job rsp weight name:{}".format(rsp_fl_job.FeatureMap(i).WeightFullname()))
-            print("start fl job rsp weight size:{}".format(len(rsp_fl_job.FeatureMap(i).DataAsNumpy())))
-    return start_fl_job_result, iteration, rsp_feature_map
+        download_compress_type = rsp_fl_job.DownloadCompressType()
+        print("start_fl_job:download_compress_type is ", compress_type_str_map[download_compress_type])
+
+        if download_compress_type == 0:
+            for i in range(0, rsp_fl_job.FeatureMapLength()):
+                rsp_feature_map[rsp_fl_job.FeatureMap(i).WeightFullname()] = rsp_fl_job.FeatureMap(i).DataAsNumpy()
+        elif download_compress_type == 2:
+            for i in range(0, rsp_fl_job.CompressFeatureMapLength()):
+                compress_feature_map = rsp_fl_job.CompressFeatureMap(i)
+                if compress_feature_map is not None:
+                    rsp_feature_map[compress_feature_map.WeightFullname()] = compress_feature_map.CompressDataAsNumpy()
+        else:
+            print("download_compress_type: {} is invalid.".format(download_compress_type))
+            sys.exit()
+    return start_fl_job_result, iteration, rsp_feature_map, rsp_fl_job.UploadCompressType(), \
+           rsp_fl_job.UploadSparseRate()
 
 
-def update_model(iteration, feature_map_temp):
+def update_model(iteration, feature_map_temp, upload_compress_type, upload_sparse_rate):
     """
     update model
     """
     update_model_result = {}
+    print("update_model:upload_compress_type is ", compress_type_str_map[upload_compress_type])
 
     url = http_type + "://" + http_server_address + '/updateModel'
-    print("Update model url:", url, ", iteration:", iteration)
-    update_model_buf, update_model_np_data = build_update_model(iteration, feature_map_temp)
+    if upload_compress_type == 0:
+        update_model_buf = build_update_model(iteration, feature_map_temp)
+    elif upload_compress_type == 1:
+        update_model_buf = build_compress_update_model(iteration, feature_map_temp, upload_compress_type,
+                                                       upload_sparse_rate)
+    else:
+        print("upload_compress_type: {} is invalid.".format(upload_compress_type))
+        sys.exit()
+    print("Update model url:", url, ", iteration:", iteration, "update_model_buf size:", len(update_model_buf))
     x = session.post(url, data=memoryview(update_model_buf).tobytes(), verify=False)
     if x.text in server_not_available_rsp:
         update_model_result['reason'] = "Restart iteration."
         update_model_result['next_ts'] = datetime_to_timestamp(datetime.datetime.now()) + 500
         print("Update model when safemode.")
-        return update_model_result, update_model_np_data
+        return update_model_result
 
     rsp_update_model = ResponseUpdateModel.ResponseUpdateModel.GetRootAsResponseUpdateModel(x.content, 0)
     if rsp_update_model.Retcode() != ResponseCode.ResponseCode.SUCCEED:
@@ -234,7 +344,7 @@ def update_model(iteration, feature_map_temp):
     else:
         update_model_result['reason'] = "Success"
         update_model_result['next_ts'] = 0
-    return update_model_result, update_model_np_data
+    return update_model_result
 
 
 def get_model(iteration):
@@ -243,16 +353,16 @@ def get_model(iteration):
     """
     get_model_result = {}
 
-    url = http_type + "://" + http_server_address  + '/getModel'
+    url = http_type + "://" + http_server_address + '/getModel'
     print("Get model url:", url, ", iteration:", iteration)
-
     while True:
-        x = session.post(url, data=memoryview(build_get_model(iteration)).tobytes(), verify=False)
+        get_model_buf = build_get_model(iteration)
+        x = session.post(url, data=memoryview(get_model_buf).tobytes(), verify=False)
         if x.text in server_not_available_rsp:
             print("Get model when safemode.")
             time.sleep(0.5)
             continue
-
+        print("Get model response size:", len(x.content))
         rsp_get_model = ResponseGetModel.ResponseGetModel.GetRootAsResponseGetModel(x.content, 0)
         ret_code = rsp_get_model.Retcode()
         if ret_code == ResponseCode.ResponseCode.SUCCEED:
@@ -263,43 +373,51 @@ def get_model(iteration):
         else:
             print("Get model failed, return code is ", rsp_get_model.Retcode())
             sys.exit()
-
-    for i in range(0, rsp_get_model.FeatureMapLength()):
-        print(rsp_get_model.FeatureMap(i).WeightFullname())
-        sys.stdout.flush()
+    download_compress_type = rsp_get_model.DownloadCompressType()
+    print("get_model:download_compress_type is ", compress_type_str_map[download_compress_type])
+    if download_compress_type == 0:
+        for i in range(0, rsp_get_model.FeatureMapLength()):
+            print("get model rsp weight name:{}".format(rsp_get_model.FeatureMap(i).WeightFullname()))
+    elif download_compress_type == 2:
+        for i in range(0, rsp_get_model.CompressFeatureMapLength()):
+            print("get model compress rsp weight name:{}".format(rsp_get_model.CompressFeatureMap(i).WeightFullname()))
+    else:
+        print("download_compress_type: {} is invalid.".format(download_compress_type))
+        sys.exit()
     print("")
     get_model_result['reason'] = "Success"
     get_model_result['next_ts'] = 0
     return get_model_result
 
 
-while True:
-    result, current_iteration, rsp_feature_maps = start_fl_job()
-    sys.stdout.flush()
-    if result['reason'] == "Restart iteration.":
-        current_ts = datetime_to_timestamp(datetime.datetime.now())
-        duration = result['next_ts'] - current_ts
-        if duration >= 0:
-            time.sleep(duration / 1000)
-        continue
+if __name__ == '__main__':
+    while True:
+        result, current_iteration, rsp_feature_maps, compress_type, sparse_rate = start_fl_job()
+        sys.stdout.flush()
+        if result['reason'] == "Restart iteration.":
+            current_ts = datetime_to_timestamp(datetime.datetime.now())
+            duration = result['next_ts'] - current_ts
+            if duration >= 0:
+                time.sleep(duration / 1000)
+            continue
 
-    result, update_data = update_model(current_iteration, rsp_feature_maps)
-    sys.stdout.flush()
-    if result['reason'] == "Restart iteration.":
-        current_ts = datetime_to_timestamp(datetime.datetime.now())
-        duration = result['next_ts'] - current_ts
-        if duration >= 0:
-            time.sleep(duration / 1000)
-        continue
+        result = update_model(current_iteration, rsp_feature_maps, compress_type, sparse_rate)
+        sys.stdout.flush()
+        if result['reason'] == "Restart iteration.":
+            current_ts = datetime_to_timestamp(datetime.datetime.now())
+            duration = result['next_ts'] - current_ts
+            if duration >= 0:
+                time.sleep(duration / 1000)
+            continue
 
-    result = get_model(current_iteration)
-    sys.stdout.flush()
-    if result['reason'] == "Restart iteration.":
-        current_ts = datetime_to_timestamp(datetime.datetime.now())
-        duration = result['next_ts'] - current_ts
-        if duration >= 0:
-            time.sleep(duration / 1000)
-        continue
+        result = get_model(current_iteration)
+        sys.stdout.flush()
+        if result['reason'] == "Restart iteration.":
+            current_ts = datetime_to_timestamp(datetime.datetime.now())
+            duration = result['next_ts'] - current_ts
+            if duration >= 0:
+                time.sleep(duration / 1000)
+            continue
 
-    print("")
-    sys.stdout.flush()
+        print("")
+        sys.stdout.flush()
